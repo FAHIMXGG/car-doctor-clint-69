3ea7f4dc1fefda2a9f80ab1d27b0cb82522a331a
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-10 14:46:24 +0600

    69 done

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
+import SignUp from "../pages/SingUp/SingUp";
 
 const router = createBrowserRouter([
     {
@@ -15,7 +16,11 @@ const router = createBrowserRouter([
         {
           path: 'login', 
           element: <Login></Login>
-      }
+        },
+        {
+          path: 'signup',
+          element: <SignUp></SignUp>
+        }
       ]
     },
   ]);
