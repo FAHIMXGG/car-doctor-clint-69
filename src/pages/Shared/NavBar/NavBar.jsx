18c8b470ba6a9a85cc64a0bafc7fffce789d6116
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-11 22:31:04 +0600

    7 logout

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const NavBar = () => {
+
+    
+    const {user, logOut} = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+        .then(() =>{})
+        .catch( error => console.log(error))
+    }
+
     const navItems = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        {/* <li><Link to="/login">Login</Link></li> */}
+        { user?.email ?  <>
+            <li><Link to="/bookings">My Bookings</Link></li>
+            <li><button onClick={handleLogOut}>Log out</button></li>
+        </> 
+        : <li> <Link to="/login">Login</Link> </li>
+       }
     </>
     return (
         <div className="navbar bg-base-100 h-24">
